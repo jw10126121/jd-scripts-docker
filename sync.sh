174cc91c65853b01f75dc94b417080f3c350fad4
linjw <10126121@qq.com>
2021-01-02 22:12:13 +0800

    更新jkd

--- sync.sh
+++ sync.sh
@@ -10,6 +10,13 @@ git clone --branch=master --depth=1 https://github.com/lxk0301/jd_scripts.git /s
   rm -rf /scripts
   mv /scripts_tmp /scripts
 }
+
+git clone --depth=1 https://github.com/shylocks/Loon.git /jkd_scripts_tmp
+[ -d /jkd_scripts_tmp ] && {
+  rm -rf /jkd_scripts_tmp
+  mv /jkd_scripts_tmp /jkd_scripts
+}
+
 cd /scripts || exit 1
 npm install || npm install --registry=https://registry.npm.taobao.org || exit 1
 [ -e /crontab.list ] && cp /crontab.list /crontab.list.old
