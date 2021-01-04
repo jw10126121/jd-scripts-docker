ec43cd05385bf1c0b8b4fc301d5de5777ea6714e
linjw <10126121@qq.com>
2021-01-04 11:22:57 +0800

    添加Sunert脚本

--- sync.sh
+++ sync.sh
@@ -18,6 +18,14 @@ git clone --branch=master --depth=1 https://github.com/lxk0301/jd_scripts.git /s
 #   mv /jkd_scripts_tmp /jkd_scripts
 # }
 
+# Sunert脚本
+git clone --depth=1 https://github.com/Sunert/Scripts.git /sunert_scripts_tmp
+[ -d /sunert_scripts_tmp ] && {
+  [ -d /sunert_scripts ] && rm -rf /sunert_scripts
+  mv /sunert_scripts_tmp /sunert_scripts
+}
+
+
 cd /scripts || exit 1
 npm install || npm install --registry=https://registry.npm.taobao.org || exit 1
 [ -e /crontab.list ] && cp /crontab.list /crontab.list.old
