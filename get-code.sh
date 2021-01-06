4fe5f0a8085db85ede4c915f57cab9e7e39fbd7b
linjw <10126121@qq.com>
2021-01-06 09:41:54 +0800

    获取邀请码

--- get-code.sh
+++ get-code.sh
@@ -1,28 +1,22 @@
-code1=
-code2=
-code3=
-code4=
-code5=
-code6=
-for srv in $(docker-compose config --services);do
-  CMD="docker logs $srv 2>/dev/null"
-  line="$(eval $CMD | egrep -A1 '您的.*(邀请码|互助码|助力码).*' | grep -v '^--$' | sed -r "s/[[:cntrl:]]\[[0-9]{1,3}m//g" | sed 'N;s/\n/ /' | sort -u )"
-  [ -z "$code1" ] || code1+=@
-  [ -z "$code2" ] || code2+=@
-  [ -z "$code3" ] || code3+=@
-  [ -z "$code4" ] || code4+=@
-  [ -z "$code5" ] || code5+=@
-  [ -z "$code6" ] || code6+=@
-  code1+=$(echo "$line" | grep "东东农场" | head -n 1 | grep -Eo '[a-zA-Z0-9_=+/]{15,}')
-  code2+=$(echo "$line" | grep "东东萌宠" | head -n 1 | grep -Eo '[a-zA-Z0-9_=+/]{15,}')
-  code3+=$(echo "$line" | grep "种豆得豆" | head -n 1 | grep -Eo '[a-zA-Z0-9_=+/]{15,}')
-  code4+=$(echo "$line" | egrep "京小超|东东超市" | head -n 1 | grep -Eo '[a-zA-Z0-9_=+/]{15,}')
-  code5+=$(echo "$line" | grep "京东赚赚" | head -n 1 | grep -Eo '[a-zA-Z0-9_=+/]{15,}')
-  code6+=$(echo "$line" | grep "健康抽奖机" | head -n 1 | grep -Eo '[a-zA-Z0-9_=+/]{15,}')
+f(){
+  a=${!1}
+  a=${a/@/}
+  echo $1=$a
+}
+for srv in jd1 jd2 jd3;do
+  line="$(docker logs $srv | grep 京东账号 | grep 互助码 | sort -u)"
+  FRUITSHARECODES+=@$(echo "$line" | grep jd_fruit.js | grep -Eo '[^】 ]*$')
+  PETSHARECODES+=@$(echo "$line" | grep jd_pet.js | grep -Eo '[^】 ]*$')
+  PLANT_BEAN_SHARECODES+=@$(echo "$line" | grep jd_plantBean.js | grep -Eo '[^】 ]*$')
+  DDFACTORY_SHARECODES+=@$(echo "$line" | grep jd_jdfactory.js | grep -Eo '[^】 ]*$')
+  DREAM_FACTORY_SHARE_CODES+=@$(echo "$line" | grep jd_dreamFactory.js | grep -Eo '[^】 ]*$')
+  JDZZ_SHARECODES+=@$(echo "$line" | grep jd_jdzz.js | grep -Eo '[^】 ]*$')
+  JDJOY_SHARECODES+=@$(echo "$line" | grep jd_crazy_joy.js | grep -Eo '[^】 ]*$')
 done
-echo FRUITSHARECODES=$code1
-echo PETSHARECODES=$code2
-echo PLANT_BEAN_SHARECODES=$code3
-echo SUPERMARKET_SHARECODES=$code4
-echo JDZZ_SHARECODES=$code5
-echo JDHealth_SHARECODES=$code6
+f FRUITSHARECODES
+f PETSHARECODES
+f PLANT_BEAN_SHARECODES
+f DDFACTORY_SHARECODES
+f DREAM_FACTORY_SHARE_CODES
+f JDZZ_SHARECODES
+f JDJOY_SHARECODES
