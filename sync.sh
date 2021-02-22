8e18664c9d7988599776a3ad604272fe01ed2d0f
linjw <10126121@qq.com>
2021-02-22 14:37:59 +0800

    更新

--- sync.sh
+++ sync.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 trap 'cp /jd-scripts-docker/sync.sh /sync' Exit
 git clone --depth=1 https://github.com/jw10126121/jd-scripts-docker.git /jd-scripts-docker_tmp
-[ -d /jd-scripts-docker_tmp ] && {
+[[ -d /jd-scripts-docker_tmp ]] && {
   echo '复制jd-scripts-docker_tmp到jd-scripts-docker'
   rm -rf /jd-scripts-docker
   mv /jd-scripts-docker_tmp /jd-scripts-docker
 
-   echo '取出codeKey'
+  echo '取出codeKey'
   cat /jd-scripts-docker/env/codeKey > /codeKey
   echo '取出sync'
   cat /jd-scripts-docker/sync.sh > /sync
@@ -17,9 +17,9 @@ git clone --depth=1 https://github.com/jw10126121/jd-scripts-docker.git /jd-scri
 # git clone --branch=master --depth=1 https://gitee.com/lxk0301/jd_scripts.git /scripts_tmp
 # [ ! -d /scripts_tmp ] && {
   # 添加KEY
-  [ -e /codeKey ] && {
+  [[ -e /codeKey ]] && {
     echo '添加公钥'
-    [ ! -d /root/.ssh ] && { mkdir -p /root/.ssh }
+    [[ ! -d /root/.ssh ]] && mkdir -p /root/.ssh
     cat /codeKey > /root/.ssh/id_rsa
     chmod 700 /root/.ssh/id_rsa
     ssh-keyscan gitee.com > /root/.ssh/known_hosts
@@ -28,7 +28,7 @@ git clone --depth=1 https://github.com/jw10126121/jd-scripts-docker.git /jd-scri
   }
 # }
 
-[ -d /scripts_tmp ] && {
+[[ -d /scripts_tmp ]] && {
   echo '覆盖新scripts'
   rm -rf /scripts
   mv /scripts_tmp /scripts
